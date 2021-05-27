56b55f376a49ad60a0981cac9a0ed309ee8f67b8
Paulo Coelho <user@PTTINFOR07.fpt.local>
2021-05-27 22:34:38 +0100

    mssql

---
+++ Superfute/src/main/java/com/mycompany/superfute/db/Dbconn.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.superfute.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+/**
+ *
+ * @author user
+ */
+
+public class Dbconn {
+    
+}
+
+
+
+    
