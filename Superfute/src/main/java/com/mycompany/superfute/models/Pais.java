0489415affbcaf6cfc9cb941b1bd61020dab6266
Nelson Duarte <1191342@isep.ipp.pt>
2021-05-27 19:33:36 +0100

    pasta de documentos, classe país e cidade

--- Superfute/src/main/java/com/mycompany/superfute/models/Pais.java
+++ Superfute/src/main/java/com/mycompany/superfute/models/Pais.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.mycompany.superfute.models;
 
 /**
  *
- * @author pcoelho
+ * @author nelso
  */
-public class Estadio {
-
+public class Pais {
     
 }
