62005ac3debdeaa2513f60d088fed7fb87f9070d
Nelson Duarte <1191342@isep.ipp.pt>
2021-06-12 18:33:33 +0100

    Criado FXML para inserir jogo e seus eventos e formações

---
+++ Superfute/src/main/java/com/mycompany/superfute/JogoFormacaoFormController.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.superfute;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+/**
+ * FXML Controller class
+ *
+ * @author nelso
+ */
+public class JogoFormacaoFormController implements Initializable {
+
+    @FXML
+    private Label labelLigaAno;
+    @FXML
+    private Button btnAddTitular;
+    @FXML
+    private Button btnRemoverTitutal;
+    @FXML
+    private Button btnAddSuplente;
+    @FXML
+    private Button btnRemoverSuplente;
+    @FXML
+    private Button btnAplicar;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private ListView<?> listViewJogadores;
+    @FXML
+    private ListView<?> listViewTitulares;
+    @FXML
+    private ListView<?> listViewSuplentes;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btnAddTitular(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnRemoverTitutal(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnAddSuplente(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnRemoverSuplente(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnAplicar(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnCancelar(ActionEvent event) {
+    }
+    
+}
